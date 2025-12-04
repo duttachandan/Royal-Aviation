f7684363ca90f6f0bdde834b020e496ba882bd2f
duttachandan <duttaankit917@gmail.com>
2025-12-04 18:34:38 +0530

    updated navbar

--- JS/index.js
+++ JS/index.js
@@ -3,3 +3,15 @@
 // primaryBtn.addEventListener("mousemove", ()=>{
 //     primaryBtn
 // })
+
+const swiper = new Swiper(".spcl-offer-slider", {
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: "24px",
+});
+
+const navbarToggler = document.querySelector(".navbar-toggler");
+
+navbarToggler.addEventListener("click", () => {
+  navbarToggler.classList.toggle("active");
+});
